6bc05b967562540fc063c03d3316bac3a88b272e
Tamim Jabr <tj222kg@student.lnu.se>
2022-01-20 00:31:44 +0100

    add data for skills

---
+++ src/data/programming-skills.tsx
@@ -0,0 +1,78 @@
+type Skill = {
+  name: string,
+  level: number,
+  color: string,
+  icon?: string,
+}
+
+
+export const skills: Skill[] = [
+  {
+    name: 'HTML',
+    level: 90,
+    color: '#f48024',
+    icon: 'https://img.icons8.com/color/48/000000/html-5.png',
+  },
+  {
+    name: 'CSS',
+    level: 90,
+    color: '#2466f4',
+    icon: 'https://img.icons8.com/color/48/000000/css3.png',
+  },
+  {
+    name: 'JavaScript',
+    level: 90,
+    color: '#f4b924',
+    icon: 'https://img.icons8.com/color/48/000000/javascript.png',
+  },
+  {
+    name: 'TypeScript',
+    level: 90,
+    color: '#24f4b9',
+    icon: 'https://img.icons8.com/color/48/000000/typescript.png',
+  },
+  {
+    name: 'React',
+    level: 90,
+    color: '#24f4b9',
+    icon: 'https://img.icons8.com/color/48/000000/react.png',
+  },
+  {
+    name: 'Redux',
+    level: 90,
+    color: '#24f4b9',
+    icon: 'https://img.icons8.com/color/48/000000/redux.png',
+  },
+  {
+    name: 'Node.js',
+    level: 90,
+    color: '#24f4b9',
+    icon: 'https://img.icons8.com/color/48/000000/nodejs.png',
+  },
+  {
+    name: 'Express',
+    level: 90,
+    color: '#24f4b9',
+    icon: 'https://img.icons8.com/color/48/000000/express.png',
+  },
+  {
+    name: 'MongoDB',
+    level: 90,
+    color: '#24f4b9',
+  },
+  {
+    name: 'Java',
+    level: 90,
+    color: '#24f4b9',
+  },
+  {
+    name: 'SQL',
+    level: 90,
+    color: '#24f4b9',
+  },
+  {
+    name: 'C#',
+    level: 90,
+    color: '#24f4b9',
+  }
+]
