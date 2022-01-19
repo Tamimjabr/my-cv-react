945f65d591b2ff6efc6a99f36739e6426a3c891e
Tamim Jabr <tj222kg@student.lnu.se>
2022-01-20 00:11:43 +0100

    delete deprected options

--- src/components/ProgrammingSkills.tsx
+++ src/components/ProgrammingSkills.tsx
@@ -30,20 +30,19 @@ const ProgrammingSkills = () => {
       colors: ['#33b2df', '#546E7A', '#d4526e', '#13d8aa', '#A5978B', '#2b908f', '#f9a3a4', '#90ee7e',
         '#f48024', '#69d2e7'
       ],
-      // dataLabels: {
-      //   enabled: true,
-      //   textAnchor: 'start',
-      //   style: {
-      //     colors: ['#fff']
-      //   },
-      //   formatter: function (val:any, opt:any) {
-      //     return opt.w.globals.labels[opt.dataPointIndex] + ":  " + val
-      //   },
-      //   offsetX: 0,
-      //   dropShadow: {
-      //     enabled: true
-      //   }
-      // },
+      dataLabels: {
+        enabled: true,
+        style: {
+          colors: ['#fff']
+        },
+        formatter: function (val: any, opt: any) {
+          return opt.w.globals.labels[opt.dataPointIndex] + ":  " + val
+        },
+        offsetX: 0,
+        dropShadow: {
+          enabled: true
+        }
+      },
       stroke: {
         width: 1,
         colors: ['#000000']
@@ -63,33 +62,27 @@ const ProgrammingSkills = () => {
         // align: 'center',
         floating: true
       },
-      // subtitle: {
-      //   text: 'Category Names as DataLabels inside bars',
-      //   align: 'center',
-      // },
-      //   tooltip: {
-      //     theme: 'dark',
-      //     x: {
-      //       show: false
-      //     },
-      //     y: {
-      //       title: {
-      //         formatter: function () {
-      //           return ''
-      //         }
-      //       }
-      //     }
-      //   }
-      // },
-    }
-
-
+      subtitle: {
+        text: 'Category Names as DataLabels inside bars',
+      },
+      tooltip: {
+        theme: 'dark',
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function () {
+              return ''
+            }
+          }
+        }
+      }
+    },
   }
-
 
   return (
     <Chart options={state.options} series={state.series} type="bar" width={800} height={500} />
-
   )
 };
 
