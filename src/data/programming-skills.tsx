96134c33a48d74285fec52d1310dc16a6d120c12
Tamim Jabr <tj222kg@student.lnu.se>
2022-01-20 09:32:46 +0100

    change the colors for programming langauges

--- src/data/programming-skills.tsx
+++ src/data/programming-skills.tsx
@@ -10,19 +10,19 @@ export const skills: Skill[] = [
   {
     name: 'HTML',
     level: 90,
-    color: '#f48024',
+    color: '#ec6630',
     icon: 'https://img.icons8.com/color/48/000000/html-5.png',
   },
   {
     name: 'CSS',
     level: 80,
-    color: '#2466f4',
+    color: '#379ad6',
     icon: 'https://img.icons8.com/color/48/000000/css3.png',
   },
   {
     name: 'JavaScript',
     level: 90,
-    color: '#f4b924',
+    color: '#ecb847',
     icon: 'https://img.icons8.com/color/48/000000/javascript.png',
   },
   {
@@ -34,39 +34,39 @@ export const skills: Skill[] = [
   {
     name: 'React',
     level: 80,
-    color: '#24f4b9',
+    color: '#7ddfff',
     icon: 'https://img.icons8.com/color/48/000000/react.png',
   },
   {
     name: 'Node.js',
     level: 90,
-    color: '#24f4b9',
+    color: '#64995f',
     icon: 'https://img.icons8.com/color/48/000000/nodejs.png',
   },
   {
     name: 'Express',
     level: 90,
-    color: '#24f4b9',
+    color: '#31312f',
     icon: 'https://img.icons8.com/color/48/000000/express.png',
   },
   {
     name: 'MongoDB',
     level: 95,
-    color: '#24f4b9',
+    color: '#13aa52',
   },
   {
     name: 'Java',
     level: 70,
-    color: '#24f4b9',
+    color: '#e51f24',
   },
   {
     name: 'SQL',
     level: 60,
-    color: '#24f4b9',
+    color: '#aabbb6',
   },
   {
     name: 'C#',
     level: 60,
-    color: '#24f4b9',
+    color: '#7f3089',
   }
 ]
