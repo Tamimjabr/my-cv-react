13cdd04a45f8de11a4528b2102f1a5c168667b45
Tamim Jabr <tj222kg@student.lnu.se>
2022-02-06 23:54:24 +0100

    style the referenses

--- src/components/References.tsx
+++ src/components/References.tsx
@@ -4,28 +4,54 @@ import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
+import { styled } from '@mui/material/styles';
+
+const StyledContainer = styled('div')`
+  padding: 1rem 0;
+  background-color: #F2FFE9;
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  & .card-holder{
+    margin: 2rem 0;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+`
+
+
 
 const References = () => {
   return (
-    <>
-      {references.map(ref => (
-        <Card sx={{ width: 300 }}>
-          <CardHeader
-            avatar={<Avatar src={ref.icon} aria-label="reference" >{ref.icon} </Avatar>}
-            title={ref.name}
-            subheader={ref.position}
-          />
-          <CardContent>
-            {ref.phone && <Typography variant="body2" color="text.secondary">
-              tel: {ref.phone}
-            </Typography>}
-            {ref.email && <Typography variant="body2" color="text.secondary">
-              e-post: {ref.email}
-            </Typography>}
-          </CardContent>
-        </Card>
-      ))}
-    </>
+    <StyledContainer>
+      <Typography variant="h4">
+        Referenser
+      </Typography>
+      <div className="card-holder">
+        {references.map(ref => (
+          <Card sx={{ width: '22rem', height: '12rem', maxWidth: 300 }}>
+            <CardHeader
+              avatar={<Avatar src={ref.icon} aria-label="reference" >{ref.icon} </Avatar>}
+              title={ref.name}
+              subheader={ref.position}
+            />
+            <CardContent>
+              {ref.phone && <Typography variant="body2" color="text.secondary">
+                tel: {ref.phone}
+              </Typography>}
+              {ref.email && <Typography variant="body2" color="text.secondary">
+                e-post: {ref.email}
+              </Typography>}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </StyledContainer>
   );
 };
 
