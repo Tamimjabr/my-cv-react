9b6cecb5ff9947a812fc80d517351d813fa7e6b9
Tamim Jabr <tj222kg@student.lnu.se>
2022-02-11 14:15:57 +0100

    style driving license part

--- src/components/DrivingLicense.tsx
+++ src/components/DrivingLicense.tsx
@@ -1,23 +1,27 @@
 import { Box, Card, CardContent, Typography } from '@mui/material'
-
+import Porsche from '../img/josh-berquist-PljkQ_KSbMc-unsplash.jpg'
 const DrivingLicense = () => {
   return (
 
     <Box
       sx={{
         minWidth: '100%',
-        height: 300,
+        height: 400,
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
         flexDirection: 'column',
-        backgroundColor: 'primary.main',
+        backgroundImage: `url(${Porsche})`, // Photo by <a href="https://unsplash.com/@bbtl?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Josh Berquist</a> on <a href="https://unsplash.com/s/photos/porsche?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
       }}
     >
-      <Typography variant="h4" sx={{ m: '0 0 1rem 0', color: 'white' }}>
+      <Typography variant="h4" sx={{ m: '0 0 rem 0', color: 'white' }}>
         Körkort
       </Typography>
-      <Card sx={{ width: '50%', textAlign: 'center' }}>
+      <Card sx={{
+        minWidth: '50%', textAlign: 'center', backgroundColor: 'rgba(255, 255, 255, 0.9)'
+      }}>
         <CardContent>
           <Typography variant="body2" sx={{ fontSize: '1.4rem' }} >
             B-körkort sedan 12-01-2016 <br />
@@ -26,7 +30,7 @@ const DrivingLicense = () => {
           </Typography>
         </CardContent>
       </Card>
-    </Box>
+    </Box >
   )
 }
 
