2eb2d24cb6107540895a6e881c0cf4a0baad3e28
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-25 00:24:45 +0100

    change the animation library

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import { styled } from '@mui/material/styles';
 import { Grid, Box, Avatar } from '@mui/material';
-import { useSpring, animated } from "react-spring";
 import ProfileImg from '../img/mypic.png';
 import ContactInfo from './ContactInfo';
 import { theme } from '../themes/material-ui-theme';
@@ -35,45 +34,31 @@ const StyledText = styled('p')`
 `
 
 const Description: FC = () => {
-  const props = useSpring({
-    delay: 300,
-    config: { duration: 500 },
-    from: {
-      opacity: 0
-    },
-    to: async (next: any) => {
-      await next({
-        opacity: 1
-      });
-    }
-  });
   return (
-    <animated.div style={props}>
-      <Box sx={{ width: '100%', height: '100%' }}>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={12} sm={12} md={4} sx={{ backgroundColor: `${theme.palette.primary.main}`, minHeight: '40vh' }}>
-            <StyledDescriptionContaioner>
-              <Avatar src={ProfileImg} alt="porfile-img" sx={{ width: 170, height: 170, m: '1rem auto' }} />
-              <StyledH4>Tamim Jabr</StyledH4>
-              <ContactInfo />
-            </StyledDescriptionContaioner>
-          </Grid>
-          <Grid item xs={12} sm={12} md={8}>
-            <StyledDescriptionContaioner sx={{ backgroundColor: 'white' }}>
-              <h3>Vem är jag?</h3>
-              <StyledText>Tamim Jabr heter jag och är 26 år, är den lagspelande problemlösaren med passion
-                för programmering. Jag är en trevlig och social person som
-                behärskar svenska, arabiska och engelska. Jag kan jobba ensamt men
-                jag jobbar hellre i en grupp där strävar man tillsammans efter ett
-                gemensamt mål. Jag kan prioritera och hålla mig lugn även när det
-                finns mycket att göra. På min fritid springer jag eller tränar på
-                gymmet.
-              </StyledText>
-            </StyledDescriptionContaioner>
-          </Grid>
+    <Box sx={{ width: '100%', height: '100%' }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item className="animate__animated animate__backInLeft" xs={12} sm={12} md={4} sx={{ backgroundColor: `${theme.palette.primary.main}`, minHeight: '40vh' }}>
+          <StyledDescriptionContaioner>
+            <Avatar src={ProfileImg} alt="porfile-img" sx={{ width: 170, height: 170, m: '1rem auto' }} />
+            <StyledH4>Tamim Jabr</StyledH4>
+            <ContactInfo />
+          </StyledDescriptionContaioner>
         </Grid>
-      </Box>
-    </animated.div>
+        <Grid item className="animate__animated animate__backInRight" xs={12} sm={12} md={8}>
+          <StyledDescriptionContaioner sx={{ backgroundColor: 'white' }}>
+            <h3>Vem är jag?</h3>
+            <StyledText>Tamim Jabr heter jag och är 26 år, är den lagspelande problemlösaren med passion
+              för programmering. Jag är en trevlig och social person som
+              behärskar svenska, arabiska och engelska. Jag kan jobba ensamt men
+              jag jobbar hellre i en grupp där strävar man tillsammans efter ett
+              gemensamt mål. Jag kan prioritera och hålla mig lugn även när det
+              finns mycket att göra. På min fritid springer jag eller tränar på
+              gymmet.
+            </StyledText>
+          </StyledDescriptionContaioner>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
