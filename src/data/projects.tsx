726089fb41ec414f94534a4fc874f0814e76e7dd
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-30 14:46:27 +0200

    add my PWA project

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -25,4 +25,10 @@ export const projects: Project[] = [{
   swedishDescription: 'En OAuth-app för Gitlab. Den tillåter användare att logga in med deras Gitlab-konto, se deras profilinformation och aktiviteter. Applikationen implementerar OAuth 2.0 utan att använda en tredje parts bibliotek som Passport.',
   URL: 'https://gitlab-oauth-nextjs-app.vercel.app',
   techniques: ['Nextjs', 'Material-UI', 'Typescript', 'Server Side Rendering', 'OAuth 2.0']
+}, {
+  title: 'PWA - Desktop App',
+  englishDescription: 'An application that uses service workers, web manifests. The Application look like a desktop and has the following applications: memory game, chat app, tic-ta-toe game, and a clock.',
+  swedishDescription: 'En applikation som använder service workers, webbmanifestfil. Applikationen ser ut som ett skrivbord och har följande applikationer: minnesspel, chattapp, tic-ta-toe-spelet, och en klocka. Appen är byggd i slutet av första terminen på webbprogrammerare programmet.',
+  URL: 'https://pwa-installable-app.netlify.app',
+  techniques: ['Html', 'CSS', 'Javascript', 'Service Workers', 'Web Manifest']
 }]
